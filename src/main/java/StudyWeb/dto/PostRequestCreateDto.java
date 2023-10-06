1a84bb16fa086271360c7317013e4134dc3cddeb
WOONBE <kepull2918@naver.com>
2023-10-06 18:09:03 +0900

    최신까지 적용

---
+++ src/main/java/StudyWeb/dto/PostRequestCreateDto.java
@@ -0,0 +1,20 @@
+package StudyWeb.dto;
+
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostRequestCreateDto {
+    private String username;
+    private String title;
+    private String content;
+
+    private List<MultipartFile> images;
+    private List<String> tags;
+}
